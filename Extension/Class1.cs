ec9a0c216c0253f4484a37f44cc94a7dd8e42fb0
ngouz <ng_dieubeni@yahoo.fr>
2019-10-15 12:34:50 +0200

    Add files via upload

---
+++ Extension/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extension
+{
+    public class Class1: ExtCore.Infrastructure.ExtensionBase
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Some name";
+            }
+        }
+    }
+
+ 
+}
